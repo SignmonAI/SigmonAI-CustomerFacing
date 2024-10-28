6fd9615d674c0a5b556a98fc95273b3c4f3322ff
AndreLuisPLuz <andreluispluz@gmail.com>
2024-10-28 14:08:45 -0300

    feat: added application specific error classes.

---
+++ core/Errors/NotFoundException.cs
@@ -0,0 +1,9 @@
+namespace core.Errors
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) {}
+
+        public NotFoundException(string message, Exception inner): base(message, inner) {}
+    }
+}
---
+++ core/Errors/UpsertException.cs
@@ -0,0 +1,9 @@
+namespace core.Errors
+{
+    public class UpsertFailException : Exception
+    {
+        public UpsertFailException(string message): base(message) { }
+
+        public UpsertFailException(string message, Exception inner): base(message, inner) { }
+    }
+}
